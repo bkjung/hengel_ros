e94d7666c5116eea8244687a174bccb427a91215
bkjung <bkjung94@gmail.com>
2018-04-25 17:02:18 +0900

    visual_odometry developing

--- hengel_slam/scripts/visual_odometry.py
+++ hengel_slam/scripts/visual_odometry.py
@@ -1,12 +1,33 @@
 #!/usr/bin/env python
 
 import rospy
+import numpy as np
+import cv2, sys, time, math
+from bird_view import calibrate_mj
 from sensor_msgs.msg import Image
+from geometry_msgs.msg import Point
+from cv_bridge import CvBridge, CvBridgeError
+from matplotlib import pyplot as plt
+
 
 class VisualOdometry():
     def __init__(self):
+        #Initialize the node with rospy
+        rospy.init_node('visual_odometry_node')
+        ################
+        rospy.on_shutdown(self.shutdown)
+        #Create publisher
+        self.vo_point_pub=rospy.Publisher("vo_current_point", Point, queue_size=50)
+        self.image_sub = rospy.Subscriber('/usb_cam/image_raw', self.callback_image_sub)
+        
+        position = Point()
+
         while True:
-            self.image_sub = rospy.Subscriber('/usb_cam/image_raw', self.callback_image_sub)
+            #####
+            
+
+    def callback_image_sub(self, _image):
+
 
 
 
