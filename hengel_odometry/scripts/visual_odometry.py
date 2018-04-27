b0699133829d6df1a0e0822efa2e9826a5af6f84
Mingg8 <mingg9688@gmail.com>
2018-04-27 12:34:40 +0900

    Add codes in odometry package

--- hengel_odometry/scripts/visual_odometry.py
+++ hengel_odometry/scripts/visual_odometry.py
@@ -3,7 +3,7 @@
 import rospy
 import numpy as np
 import cv2, sys, time, math
-from bird_view import calibrate_mj
+# from bird_view import calibrate_mj
 from sensor_msgs.msg import Image
 from geometry_msgs.msg import Point
 from cv_bridge import CvBridge, CvBridgeError
@@ -20,22 +20,25 @@ class VisualOdometry():
         self.vo_point_pub=rospy.Publisher("vo_current_point", Point, queue_size=50)
         self.image_sub = rospy.Subscriber('/usb_cam/image_raw', self.callback_image_sub)
         
-        position = Point()
+        birdview_publisher.birdview_publisher(image_sub)
+        
 
-        while True:
-            #####
+
+        position = Point()
+        rospy.spin()
+
             
 
     def callback_image_sub(self, _image):
-
-
+        bridge= CvBridge()
+        return bridge.imgmsg_to_cv2(_image)
 
 
 if __name__ == '__main__':
     try:
         VisualOdometry()
 
-        print("End of Main Function)
+        print("End of Main Function")
 
     except Exception as e:
         print(e)
