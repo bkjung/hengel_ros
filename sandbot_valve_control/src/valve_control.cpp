ebcd9454fc9c56a887bbf38669291a3ac39c76e6
hskim617 <hskim617@gmail.com>
2018-04-30 18:49:40 +0900

    made sandbot_valve_control pkg

---
+++ sandbot_valve_control/src/valve_control.cpp
@@ -0,0 +1,314 @@
+/*******************************************************************************
+* Copyright 2017 ROBOTIS CO., LTD.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
+
+/* Author: Ryu Woon Jung (Leon) */
+
+//
+// *********     Read and Write Example      *********
+//
+//
+// Available Dynamixel model on this example : All models using Protocol 2.0
+// This example is tested with a Dynamixel PRO 54-200, and an USB2DYNAMIXEL
+// Be sure that Dynamixel PRO properties are already set as %% ID : 1 / Baudnum : 1 (Baudrate : 57600)
+//
+
+#if defined(__linux__) || defined(__APPLE__)
+#include <fcntl.h>
+#include <termios.h>
+#define STDIN_FILENO 0
+#elif defined(_WIN32) || defined(_WIN64)
+#include <conio.h>
+#endif
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+// #include "sandbot_valve_control/DynamixelSDK.h"                                  // Uses Dynamixel SDK library
+#include "ros/ros.h"
+#include "sandbot_valve_control/ValveInput.h"
+#include "sandbot_valve_control/MotorInput.h"
+#include "sandbot_valve_control/packet_handler.h"
+#include "sandbot_valve_control/port_handler.h"
+
+// Control table address
+#define ADDR_PRO_TORQUE_ENABLE          64                 // Control table address is different in Dynamixel model
+#define ADDR_PRO_GOAL_POSITION          116
+#define ADDR_PRO_PRESENT_POSITION       132
+#define ADDR_PRO_GOAL_VELOCITY          104
+#define ADDR_PRO_PRESENT_VELOCITY       128
+#define ADDR_PRO_OPERATING_MODE         11
+
+
+// Protocol version
+#define PROTOCOL_VERSION                2.0                 // See which protocol version is used in the Dynamixel
+
+// Default setting
+#define DXL_ID_10                       10                   // Dynamixel ID: 1
+#define DXL_ID_13                       13
+#define DXL_ID_254                      254
+#define BAUDRATE                        57600
+#define DEVICENAME                      "/dev/ttyUSB0"      // Check which port is being used on your controller
+                                                            // ex) Windows: "COM1"   Linux: "/dev/ttyUSB0" Mac: "/dev/tty.usbserial-*"
+
+#define TORQUE_ENABLE                   1                   // Value for enabling the torque
+#define TORQUE_DISABLE                  0                   // Value for disabling the torque
+// #define DXL_MINIMUM_POSITION_VALUE      512            // Dynamixel will rotate between this value
+// #define DXL_MAXIMUM_POSITION_VALUE      1023              // and this value (note that the Dynamixel would not move when the position value is out of movable range. Check e-manual about the range of the Dynamixel you use.)
+#define DXL_MOVING_STATUS_THRESHOLD     10                  // Dynamixel moving status threshold
+#define VELOCITY_CONTROL_MODE           1
+#define DXL_MAXIMUM_VELOCITY            380
+int GOAL_POSITION;
+
+#define ESC_ASCII_VALUE                 0x1b
+
+int getch()
+{
+#if defined(__linux__) || defined(__APPLE__)
+  struct termios oldt, newt;
+  int ch;
+  tcgetattr(STDIN_FILENO, &oldt);
+  newt = oldt;
+  newt.c_lflag &= ~(ICANON | ECHO);
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+  ch = getchar();
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+  return ch;
+#elif defined(_WIN32) || defined(_WIN64)
+  return _getch();
+#endif
+}
+//
+// int kbhit(void)
+// {
+// #if defined(__linux__) || defined(__APPLE__)
+//   struct termios oldt, newt;
+//   int ch;
+//   int oldf;
+//
+//   tcgetattr(STDIN_FILENO, &oldt);
+//   newt = oldt;
+//   newt.c_lflag &= ~(ICANON | ECHO);
+//   tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+//   oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+//   fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+//
+//   ch = getchar();
+//
+//   tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+//   fcntl(STDIN_FILENO, F_SETFL, oldf);
+//
+//   if (ch != EOF)
+//   {
+//     ungetc(ch, stdin);
+//     return 1;
+//   }
+//
+//   return 0;
+// #elif defined(_WIN32) || defined(_WIN64)
+//   return _kbhit();
+// #endif
+// }
+
+void changeValveInput(int goal_position)
+{
+  printf("Changed goal position");
+  GOAL_POSITION = goal_position;
+}
+
+void msgCallback(const sandbot_valve_control::ValveInput::ConstPtr& msg1)
+{
+  ROS_INFO("recieve msg1 = %d", msg1->goal_position);
+  changeValveInput(msg1->goal_position);
+}
+
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "valve_control");
+  ros::NodeHandle nh;
+  ros::Publisher pub = nh.advertise<sandbot_valve_control::MotorInput>("motor_input", 100);
+
+  sandbot_valve_control::MotorInput msg2;
+
+  ros::Rate loop_rate(10);
+
+
+  // Initialize PortHandler instance
+  // Set the port path
+  // Get methods and members of PortHandlerLinux or PortHandlerWindows
+  dynamixel::PortHandler *portHandler = dynamixel::PortHandler::getPortHandler(DEVICENAME);
+
+  // Initialize PacketHandler instance
+  // Set the protocol version
+  // Get methods and members of Protocol1PacketHandler or Protocol2PacketHandler
+  dynamixel::PacketHandler *packetHandler = dynamixel::PacketHandler::getPacketHandler(PROTOCOL_VERSION);
+
+  // int index = 0;
+  int dxl_comm_result = COMM_TX_FAIL;             // Communication result
+  // int dxl_goal_position[2] = {DXL_MINIMUM_POSITION_VALUE, DXL_MAXIMUM_POSITION_VALUE};         // Goal position
+  int dxl_goal_velocity = DXL_MAXIMUM_VELOCITY;
+
+
+  uint8_t dxl_error = 0;                          // Dynamixel error
+  int32_t dxl_present_position = 0;               // Present position
+
+  // Open port
+  if (portHandler->openPort())
+  {
+    printf("Succeeded to open the port!\n");
+  }
+  else
+  {
+    printf("Failed to open the port!\n");
+    printf("Press any key to terminate...\n");
+    getch();
+    return 0;
+  }
+
+  // Set port baudrate
+  if (portHandler->setBaudRate(BAUDRATE))
+  {
+    printf("Succeeded to change the baudrate!\n");
+  }
+  else
+  {
+    printf("Failed to change the baudrate!\n");
+    printf("Press any key to terminate...\n");
+    getch();
+    return 0;
+  }
+
+  // Change to Wheel model
+  dxl_comm_result = packetHandler->write1ByteTxRx(portHandler, DXL_ID_10, ADDR_PRO_OPERATING_MODE, VELOCITY_CONTROL_MODE, &dxl_error);
+  if (dxl_comm_result != COMM_SUCCESS)
+  {
+    // printf("%s\n", packetHandler->getTxRxResult(dxl_comm_result));
+    printf("%s\n", packetHandler->getTxRxResult(dxl_comm_result));
+  }
+  else if (dxl_error != 0)
+  {
+    // printf("%s\n", packetHandler->getRxPacketError(dxl_error));
+    printf("%s\n", packetHandler->getRxPacketError(dxl_error));
+  }
+  else
+  {
+    printf("Dynamixel ID_10 is now on wheel mode \n");
+  }
+
+  // Enable Dynamixel Torque
+  dxl_comm_result = packetHandler->write1ByteTxRx(portHandler, DXL_ID_254, ADDR_PRO_TORQUE_ENABLE, TORQUE_ENABLE, &dxl_error);
+  if (dxl_comm_result != COMM_SUCCESS)
+  {
+    // printf("%s\n", packetHandler->getTxRxResult(dxl_comm_result));
+    printf("%s\n", packetHandler->getTxRxResult(dxl_comm_result));
+  }
+  else if (dxl_error != 0)
+  {
+    // printf("%s\n", packetHandler->getRxPacketError(dxl_error));
+    printf("%s\n", packetHandler->getRxPacketError(dxl_error));
+  }
+  else
+  {
+    printf("Dynamixel has been successfully connected \n");
+  }
+
+  printf("while loop start");
+
+
+
+  ROS_INFO("abc");
+  ros::Subscriber sub = nh.subscribe("valve_input", 100, msgCallback);
+  // printf("Press any key to continue! (or press ESC to quit!)\n");
+  // if (getch() == ESC_ASCII_VALUE)
+  //   break;
+
+  // Write goal velocity
+  dxl_comm_result = packetHandler->write4ByteTxRx(portHandler, DXL_ID_10, ADDR_PRO_GOAL_VELOCITY, dxl_goal_velocity, &dxl_error);
+  if (dxl_comm_result != COMM_SUCCESS)
+  {
+    printf("%s\n", packetHandler->getTxRxResult(dxl_comm_result));
+  }
+  else if (dxl_error != 0)
+  {
+    printf("%s\n", packetHandler->getRxPacketError(dxl_error));
+  }
+
+  // Write goal position
+  dxl_comm_result = packetHandler->write4ByteTxRx(portHandler, DXL_ID_13, ADDR_PRO_GOAL_POSITION, GOAL_POSITION, &dxl_error);
+  if (dxl_comm_result != COMM_SUCCESS)
+  {
+    printf("%s\n", packetHandler->getTxRxResult(dxl_comm_result));
+  }
+  else if (dxl_error != 0)
+  {
+    printf("%s\n", packetHandler->getRxPacketError(dxl_error));
+  }
+  do
+  {
+    // Read present position
+    dxl_comm_result = packetHandler->read4ByteTxRx(portHandler, DXL_ID_13, ADDR_PRO_PRESENT_POSITION, (uint32_t*)&dxl_present_position, &dxl_error);
+    if (dxl_comm_result != COMM_SUCCESS)
+    {
+      printf("%s\n", packetHandler->getTxRxResult(dxl_comm_result));
+    }
+    else if (dxl_error != 0)
+    {
+      printf("%s\n", packetHandler->getRxPacketError(dxl_error));
+    }
+
+    printf("[ID:%03d] GoalPos:%03d  PresPos:%03d\n", DXL_ID_13, GOAL_POSITION, dxl_present_position);
+
+  }while((abs(GOAL_POSITION - dxl_present_position) > DXL_MOVING_STATUS_THRESHOLD));
+
+  // // Change goal position
+  // if (index == 0)
+  // {
+  //   index = 1;
+  // }
+  // else
+  // {
+  //   index = 0;
+  // }
+  msg2.present_position = dxl_present_position;
+
+  pub.publish(msg2);
+  loop_rate.sleep();
+
+  ros::spin();
+
+
+  // }
+  /////////////////////////////////WHILE LOOP END////////////////////////////////////////////
+
+
+  // Disable Dynamixel Torque
+  dxl_comm_result = packetHandler->write1ByteTxRx(portHandler, DXL_ID_254, ADDR_PRO_TORQUE_ENABLE, TORQUE_DISABLE, &dxl_error);
+  if (dxl_comm_result != COMM_SUCCESS)
+  {
+    printf("%s\n", packetHandler->getTxRxResult(dxl_comm_result));
+  }
+  else if (dxl_error != 0)
+  {
+    printf("%s\n", packetHandler->getRxPacketError(dxl_error));
+  }
+
+
+  // Close port
+  portHandler->closePort();
+
+  return 0;
+}
