494f7b49f0da13da8617e50b4c2918a770095150
bkjung <bkjung94@gmail.com>
2018-05-01 12:50:00 +0900

    folder directory structure changed && file path import method changed

--- hengel_path_manager/scripts/pathmaker_ui.py
+++ hengel_path_manager/scripts/pathmaker_ui.py
@@ -13,23 +13,26 @@ circle_array=[]
 size_x=500
 size_y=540
 dist=0.02415
-radius=10
+radius=2
 global isDrawmode
 isDrawmode = False
+
+package_base_path = os.path.abspath(os.path.join(os.path.dirname(__file__),"../.."))
+os.system("mkdir -p "+package_base_path+"/hengel_path_manager/path_made")
 
 def callback(event):
     if isDrawmode:
         circle=c.create_oval(event.x-radius, event.y-radius, event.x+radius, event.y+radius, width=2, fill='black')
         circle_array.append(circle)
-        write(float(event.x)/size_x, float(event.y)/size_y)
+        path_append(float(event.x)/size_x, float(event.y)/size_y)
 
-def write(x,y):
+def path_append(x,y):
     last_x, last_y=path[-1]
     if sqrt(pow(last_x-x,2)+pow(last_y-y,2))>dist:
         path.append([x,y])
         print(x,y)
 
-def drawing():
+def command_drawing():
     global isDrawmode
     if button_draw.config('relief')[-1] == 'sunken':
         button_draw.config(relief="raised")
@@ -39,19 +42,21 @@ def drawing():
         isDrawmode=True
     return
 
-def cancel():
+def command_cancel():
     c.delete(circle_array[-1])
     return
 
-def save():
-    f=open("./txtfile/pathmaker_"+time.strftime("%y%m%d_%H%M%S")+".txt",'w')
+def command_save():
+    save_path = package_base_path+"/hengel_path_manager/path_made/"+time.strftime("%y%m%d_%H%M%S")+".txt"
+    f=open(save_path,'w')
     for i in range(1,len(path)):
         data=str(path[i][0])+"  "+str(path[i][1])+"\n"
         f.write(data)
     f.close()
+    print("path saved at " + save_path)
     return
 
-def quit():
+def command_quit():
     global root
     root.quit()
 
@@ -59,13 +64,12 @@ def quit():
 root = Tk()
 if __name__ =="__main__":
     root.title("Path")
-    os.system("mkdir -p txtfile")
     # img=Image.open("./alphabet_imgs/"+filename+".jpeg")
     # img.thumbnail(size, Image.ANTIALIAS)
     # photo=ImageTk.PhotoImage(img)
 
     # l=Label(root, image=photo)
-    # l.pack()
+    # l.pack()cancel
     _str=str(size_x)+"x"+str(size_y)
     root.geometry(_str)
 
@@ -76,16 +80,16 @@ if __name__ =="__main__":
     f=Frame(root)
     f.grid(row=0, column=0, sticky="n")
 
-    button_draw=Button(f, text="Draw", relief="raised", command=drawing)
+    button_draw=Button(f, text="Draw", relief="raised", command=command_drawing)
     button_draw.grid(row=0, column=0)
 
-    button_cancel=Button(f, text="Cancel", command=cancel)
+    button_cancel=Button(f, text="Cancel", command=command_cancel)
     button_cancel.grid(row=0, column=1)
 
-    button_save=Button(f, text="Save", command=save)
+    button_save=Button(f, text="Save", command=command_save)
     button_save.grid(row=0, column=2)
 
-    button_quit=Button(f, text="Quit", command=quit)
+    button_quit=Button(f, text="Quit", command=command_quit)
     button_quit.grid(row=0, column=3)
 
     # label_entry=Entry(f, width=10)
