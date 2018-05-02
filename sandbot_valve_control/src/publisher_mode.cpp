32007700b389ee3b6dc595d7ae0e7e2896b04691
hskim617 <hskim617@gmail.com>
2018-05-02 15:42:51 +0900

    Add message file

---
+++ sandbot_valve_control/src/publisher_mode.cpp
@@ -0,0 +1,26 @@
+#include "ros/ros.h"
+#include "sandbot_valve_control/OperationMode.h"
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "valve_input_publisher_mode");
+  ros::NodeHandle nh;
+
+  ros::Publisher pub=nh.advertise<sandbot_valve_control::OperationMode>("operation_mode", 10);
+  ros::Rate loop_rate(1);
+
+  sandbot_valve_control::OperationMode msg_mode;
+
+  bool isContinuous= false;
+  while(ros::ok()){
+    if(isContinuous==true){
+      msg_mode.mode=0;
+    }
+    else{
+      msg_mode.mode=1;
+    }
+    pub.publish(msg_mode);
+    loop_rate.sleep();
+  }
+  return 0;
+}
