3c0c2e946feec8d261558694cf462cab63432b81
bkjung <bkjung94@gmail.com>
2018-05-19 17:30:08 +0900

    waypoint data to multi-dimensional array (removed starting_index array)

--- hengel_navigation/scripts/paint_letter.py
+++ hengel_navigation/scripts/paint_letter.py
@@ -34,31 +34,30 @@ class PaintLetter():
     def get_path(self):
         dir_1= package_base_path+"/hengel_path_manager/alphabet_path/path_"
         dir_2=".txt"
-        # cnt_letter = 0
-        # cnt_waypoint = 0
+        letter_index = 0
         for letter in self.word:
             letter_path=[]
             if letter==' ':
                 pass
                 #if spacing is included
             else:
-                # self.draw_start_index.append(cnt_waypoint)
                 with open(dir_1+letter.capitalize()+dir_2,"r") as file_path:
                     for idx, line in enumerate(file_path):
                         _str = line.split()
                         if not len(_str)==0:
-                            # self.arr_path.append([(float)(_str[0])+(float)(cnt_letter)-(2*(float)(cnt_letter)-1)*250/1632, 1.0-(float)(_str[1])])
-                            # cnt_waypoint = cnt_waypoint + 1
-                            letter_path.append([(float)(_str[0])+(float)(cnt_letter)-(2*(float)(cnt_letter)-1)*250/1632, 1.0-(float)(_str[1])])
+                            #letter_path.append([(float)(_str[0])+(float)(letter_index)-(2*(float)(letter_index)-1)*250/1632, 1.0-(float)(_str[1])])
+                            letter_path.append([(float)(_str[0])+(float)(letter_index), 1.0-(float)(_str[1])])
                         else:
                             pass
+            #count the number of letters including spacing
+            letter_index = letter_index + 1
+
             self.arr_path.append(letter_path)
+
         arr_path_file =cv2.FileStorage(package_base_path+"/hengel_path_manager/waypnts/Path.xml", cv2.FILE_STORAGE_WRITE)
         arr_path_file.write("arr_path", self.arr_path)
         arr_path_file.release()
-        #count the number of letters including spacing
-        # cnt_letter = cnt_letter + 1
-    
+            
     def run(self):
         # NavigationControl(self.arr_path, self.draw_start_index)
         NavigationControl(self.arr_path)
