c4e215a474ebbfe29bec86d9f9f2fa9d90209158
bkjung <bkjung94@gmail.com>
2018-05-22 01:55:12 +0900

    developing

--- hengel_navigation/scripts/paint_letter.py
+++ hengel_navigation/scripts/paint_letter.py
@@ -13,20 +13,20 @@ import time
 import os
 from navigation_control import NavigationControl
 import cv2
-
+import main_program as mp
 
 
 package_base_path = os.path.abspath(os.path.join(os.path.dirname(__file__),"../.."))
 os.system("mkdir -p "+package_base_path+"/hengel_path_manager/output_pathmap")
-os.system("mkdir -p"+package_base_path+"/hengel_path_manager/waypnts")
+os.system("mkdir -p "+package_base_path+"/hengel_path_manager/waypnts")
 
 
 class PaintLetter():
     def __init__(self):
+        print("Length of Canvas Side = "+str(mp.CANVAS_SIDE_LENGTH))
         self.arr_path=[]
         self.draw_start_index=[]
         self.word=raw_input("Type letters to draw:")
-        print("Input:", self.word)
 
         self.get_path()
         self.run()
@@ -46,7 +46,7 @@ class PaintLetter():
                         _str = line.split()
                         if not len(_str)==0:
                             #letter_path.append([(float)(_str[0])+(float)(letter_index)-(2*(float)(letter_index)-1)*250/1632, 1.0-(float)(_str[1])])
-                            letter_path.append([(float)(_str[0])+(float)(letter_index), 1.0-(float)(_str[1])])
+                            letter_path.append([((float)(_str[0])+(float)(letter_index))*mp.CANVAS_SIDE_LENGTH, (1.0-(float)(_str[1]))*mp.CANVAS_SIDE_LENGTH])
                         else:
                             pass
             #count the number of letters including spacing
