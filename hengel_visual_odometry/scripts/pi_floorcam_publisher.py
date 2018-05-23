9e735a6bceaee9438cd6fde0ad2a4bb9bd1b4dbf
bkjung <bkjung94@gmail.com>
2018-05-23 16:47:20 +0900

    pi_floorcam enhanced to publish compressed image

--- hengel_visual_odometry/scripts/pi_floorcam_publisher.py
+++ hengel_visual_odometry/scripts/pi_floorcam_publisher.py
@@ -3,18 +3,24 @@ import rospy
 import numpy as np
 import cv2, sys, time, math
 from sensor_msgs.msg import Image
+from sensor_msgs.msg import CompressedImage
 from cv_bridge import CvBridge
 
 if __name__ == '__main__':
     cap=cv2.VideoCapture(0)
-    pub=rospy.Publisher('/pi_floorcam/image_raw', Image, queue_size=10)
+    pub=rospy.Publisher('/pi_floorcam/image_raw/compressed', CompressedImage, queue_size = 3)
     rospy.init_node('pi_floorcam_node', anonymous= True)
     rate=rospy.Rate(10)
 
     bridge=CvBridge()
     while not rospy.is_shutdown():
         ret,frame=cap.read()
-        pub.publish(bridge.cv2_to_imgmsg(frame, "bgr8"))
+	msg = CompressedImage()
+	msg.format = "jpeg"
+	msg.data = np.array(cv2.imencode('.jpg', frame)[1]).tostring()
+	#publish new image
+	pub.publish(msg) 
+        #pub.publish(bridge.cv2_to_imgmsg(frame, "bgr8"))
         cv2.waitKey(100)
     
     cap.release()
