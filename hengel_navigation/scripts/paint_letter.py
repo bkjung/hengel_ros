c2269dd82f291c4f57f711a39f7f69afb3af5283
bkjung <bkjung94@gmail.com>
2018-06-28 14:21:04 +0900

    Error debug

--- hengel_navigation/scripts/paint_letter.py
+++ hengel_navigation/scripts/paint_letter.py
@@ -14,7 +14,7 @@ import os
 from navigation_control import NavigationControl
 import cv2
 
-CANVAS_SIDE_LENGTH = 1.5 * 0.58
+CANVAS_SIDE_LENGTH = 0.5 * 0.58
 #CANVAS_SIDE_LENGTH = 0.5 * 0.58
 #PADDING_LENGTH = 0.0
 PADDING_LENGTH = -0.65 * 0.58
