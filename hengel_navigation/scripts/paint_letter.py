ae6652cefdcb7977782af3071d7170e25503a07e
bkjung <bkjung94@gmail.com>
2018-07-18 11:28:18 +0900

    minor fix

--- hengel_navigation/scripts/paint_letter.py
+++ hengel_navigation/scripts/paint_letter.py
@@ -51,7 +51,7 @@ class PaintLetter():
 
         if self.isPositionControl:
             while True:
-                word1=raw_input("Type the offset of the applicator (0.18 for test flag bot): ")
+                word1=raw_input("Type the offset of the applicator (0.20 for test flag bot): ")
 
                 self.D=float(word1)
                 if self.D==0:
