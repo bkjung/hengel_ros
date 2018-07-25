2446d906ed8fd2c137f0811135bf7a55b551f167
Minji Lee <mingg9688@gmail.com>
2018-07-25 15:18:12 +0900

    developing

--- hengel_camera/scripts/picam_publisher_1.py
+++ hengel_camera/scripts/picam_publisher_1.py
@@ -11,19 +11,19 @@ from cv_bridge import CvBridge
 
 package="/home/turtleberry/catkin_ws/src/hengel_ros/hengel_camera/"
 if __name__=='__main__':
-	rospy.init_node('wide_cam_node_1', anonymous=True)
-	pub = rospy.Publisher('/wide_cam_1/compressed', CompressedImage, queue_size=1)
-	pub2= rospy.Publisher('/wide_cam_2/compressed', CompressedImage, queue_size=1)
+	rospy.init_node('genius_publisher_2', anonymous=True)
+	pub = rospy.Publisher('/genius1/compressed', CompressedImage, queue_size=1)
+	pub2= rospy.Publisher('/genius2/compressed', CompressedImage, queue_size=1)
 	rate = rospy.Rate(10)
 	
-	cam=cv2.VideoCapture(1)
-	cam2=cv2.VideoCapture(2)
+	cam=cv2.VideoCapture(0)
+	#cam2=cv2.VideoCapture(1)
 	cam.set(cv2.CAP_PROP_FRAME_WIDTH,960)
 	cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 540)
 	cam.set(cv2.CAP_PROP_FOURCC, int(0x47504A4D))
-	cam2.set(cv2.CAP_PROP_FRAME_WIDTH,960)
-	cam2.set(cv2.CAP_PROP_FRAME_HEIGHT, 540)
-	cam2.set(cv2.CAP_PROP_FOURCC, int(0x47504A4D))
+	#cam2.set(cv2.CAP_PROP_FRAME_WIDTH,960)
+	#cam2.set(cv2.CAP_PROP_FRAME_HEIGHT, 540)
+	#cam2.set(cv2.CAP_PROP_FOURCC, int(0x47504A4D))
 
 	while True:
        	    if rospy.is_shutdown():
@@ -45,12 +45,14 @@ if __name__=='__main__':
     	    #		msg=bridge.cv2_to_compressed_imgmsg(image)
     	    #		pub.publish(msg)
 	    bridge=CvBridge()
-	    _, img=cam.read()
-	    _, img2=cam2.read()
-	    msg1=bridge.cv2_to_compressed_imgmsg(img)
-	    msg2=bridge.cv2_to_compressed_imgmsg(img2)
-	    cv2.imshow("cam", img)
-	    cv2.imshow("cam2",img2)
-	    pub.publish(msg1)
-	    pub2.publish(msg2)
+	    if cam.isOpened():
+	        _, img=cam.read()
+	        msg1=bridge.cv2_to_compressed_imgmsg(img)
+	        #cv2.imshow("cam", img)
+	    #cv2.imshow("cam2",img2)
+	        pub.publish(msg1)
+	    #if cam2.isOpened():
+	    #    _, img2=cam2.read()
+	    #    msg2=bridge.cv2_to_compressed_imgmsg(img2)
+	    #    pub2.publish(msg2)
 	    cv2.waitKey(3)
