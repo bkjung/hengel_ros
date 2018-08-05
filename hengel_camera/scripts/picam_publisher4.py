b1280692b0bdf82570ada8a4d5160d1874595dc2
Minji Lee <mingg9688@gmail.com>
2018-08-05 19:40:48 +0900

    buffer problem

--- hengel_camera/scripts/picam_publisher4.py
+++ hengel_camera/scripts/picam_publisher4.py
@@ -16,19 +16,22 @@ class CamPublisher():
 	def __init__(self):
 		rospy.init_node('genius_publisher_1', anonymous=True)
 
-		self.cam=cv2.VideoCapture(0)
-		self.cam2=cv2.VideoCapture(1)
-		self.cam.set(cv2.CAP_PROP_FRAME_WIDTH,960)
-		self.cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 540)
+		self.cam=cv2.VideoCapture(1)
+		self.cam2=cv2.VideoCapture(2)
+
+		self.cam.set(cv2.CAP_PROP_FRAME_WIDTH,800)
+		self.cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 600)
 		self.cam.set(cv2.CAP_PROP_FOURCC, int(0x47504A4D))
-		self.cam2.set(cv2.CAP_PROP_FRAME_WIDTH,960)
-		self.cam2.set(cv2.CAP_PROP_FRAME_HEIGHT, 540)
+		self.cam2.set(cv2.CAP_PROP_FRAME_WIDTH,800)
+		self.cam2.set(cv2.CAP_PROP_FRAME_HEIGHT, 600)
 		self.cam2.set(cv2.CAP_PROP_FOURCC, int(0x47504A4D))
 		
 		rospy.Subscriber('/initiator', Bool, self.initiator)
 		self.pub = rospy.Publisher('/genius1/compressed', CompressedImage, queue_size=10)
 		self.pub2= rospy.Publisher('/genius2/compressed', CompressedImage, queue_size=10)
 		self.rate = rospy.Rate(5)
+
+		self.publish()
 		rospy.spin()
 	
 	def initiator(self,msg):
@@ -59,6 +62,10 @@ class CamPublisher():
 			#		msg=bridge.cv2_to_compressed_imgmsg(image)
 			#		pub.publish(msg)
 			bridge=CvBridge()
+
+			for i in xrange(4):
+				self.cam.grab()
+				self.cam2.grab()
 			_, img=self.cam.read()
 			_, img2=self.cam2.read()
 			msg1=bridge.cv2_to_compressed_imgmsg(img)
