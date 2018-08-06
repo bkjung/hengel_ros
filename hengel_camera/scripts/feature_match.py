5a36e14b75d1c9eba74a939a0c66b25a4972daaf
bkjung <bkjung94@gmail.com>
2018-08-06 16:28:48 +0900

    Homography mtx M returned

--- hengel_camera/scripts/feature_match.py
+++ hengel_camera/scripts/feature_match.py
@@ -116,37 +116,48 @@ class FeatureMatch():
             src_pts=np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
             dst_pts=np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
 
+
+
+            print("DEBUG-0")
             M, mask= cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+            if M==None:
+                print("Homography mtx M is None !!!!")
+            else:
+                print(M)
 
-            img1_homo=cv2.warpPerspective(img1, M, (1280, 1280))
-            img2_homo=cv2.warpPerspective(img2, M, (1280, 1280))
+                # print("DEBUG-1")
+                # img1_homo=cv2.warpPerspective(img1, M, (1280, 1280))
+                # print("DEBUG-2")
+                # img2_homo=cv2.warpPerspective(img2, M, (1280, 1280))
+                # print("DEBUG-3")
 
-            # cv2.imwrite("/home/bkjung/img1_homo.png", img1_homo)
-            # cv2.imwrite("/home/bkjung/img2_homo.png", img2_homo)
-            # matchesMask=mask.ravel().tolist()
+                # cv2.imwrite("/home/bkjung/img1_homo.png", img1_homo)
+                # cv2.imwrite("/home/bkjung/img2_homo.png", img2_homo)
+                # matchesMask=mask.ravel().tolist()
 
-            # h,w = img1.shape
-            # pts=np.float32([[0,0],[0,h-1],[w-1,h-1], [w-1,0]]).reshape(-1,1,2)
-            # dst=cv2.perspectiveTransform(pts,M)
+                # h,w = img1.shape
+                # pts=np.float32([[0,0],[0,h-1],[w-1,h-1], [w-1,0]]).reshape(-1,1,2)
+                # dst=cv2.perspectiveTransform(pts,M)
 
 
-            # Need to draw only good matches, so create a mask
-            matchesMask = [[0,0] for i in xrange(len(matches))]
-            # ratio test as per Lowe's paper
-            for i,(m,n) in enumerate(matches):
-                if m.distance < 0.7*n.distance:
-                    matchesMask[i]=[1,0]
-            draw_params = dict(matchColor = (0,255,0),
-                            singlePointColor = (255,0,0),
-                            matchesMask = matchesMask,
-                            flags = 0)
-            img3 = cv2.drawMatchesKnn(img2,kp2,img1,kp1,matches,None,**draw_params)
+                # Need to draw only good matches, so create a mask
+                matchesMask = [[0,0] for i in xrange(len(matches))]
+                # ratio test as per Lowe's paper
+                for i,(m,n) in enumerate(matches):
+                    if m.distance < 0.7*n.distance:
+                        matchesMask[i]=[1,0]
+                draw_params = dict(matchColor = (0,255,0),
+                                singlePointColor = (255,0,0),
+                                matchesMask = matchesMask,
+                                flags = 0)
+                img3 = cv2.drawMatchesKnn(img2,kp2,img1,kp1,matches,None,**draw_params)
 
-            # cv2.imwrite(self.folder_path+"/SIFT_FLANN_MATCH_"+time.strftime("%y%m%d_%H%M%S")+".png", img3)
+                print("DEBUG-4")
 
-            plt.subplot(313)
-            plt.imshow(img3, cmap='gray')
-            print("done")
+                # cv2.imwrite(self.folder_path+"/SIFT_FLANN_MATCH_"+time.strftime("%y%m%d_%H%M%S")+".png", img3)
+
+                plt.subplot(313)
+                plt.imshow(img3, cmap='gray')
         else:
             print("Feature Match FAILED")
 
@@ -156,6 +167,8 @@ class FeatureMatch():
         # plt.pause(0.00000000001)
 
         print("sift_falnn 4 Time: "+str(time.time()-_time))
+
+        return M
 
 if __name__=="__main__":
     img_virtual= cv2.imread("/home/bkjung/demo_1280.png", cv2.IMREAD_GRAYSCALE)
