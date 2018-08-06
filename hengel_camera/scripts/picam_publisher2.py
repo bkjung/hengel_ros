24b932763301f76bf1c9969831b596c30cae9ca1
Mingg8 <mingg9688@gmail.com>
2018-08-06 14:00:37 +0900

    developing

--- hengel_camera/scripts/picam_publisher2.py
+++ hengel_camera/scripts/picam_publisher2.py
@@ -17,20 +17,13 @@ class CamPublisher():
 		rospy.init_node('genius_publisher2', anonymous=True)
 
 		
-<<<<<<< HEAD
 		self.cam=cv2.VideoCapture(1)
 		self.cam2=cv2.VideoCapture(2)
 		self.cam.set(cv2.CAP_PROP_FRAME_WIDTH,800)
 		self.cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 600)
-=======
-		self.cam=cv2.VideoCapture(0)
-		self.cam2=cv2.VideoCapture(1)
-		self.cam.set(cv2.CAP_PROP_FRAME_WIDTH,960)
-		self.cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 540)
->>>>>>> ce6f47b43966ab7e5ad35b1f7c82a5887bdf43f5
 		self.cam.set(cv2.CAP_PROP_FOURCC, int(0x47504A4D))
-		self.cam2.set(cv2.CAP_PROP_FRAME_WIDTH,960)
-		self.cam2.set(cv2.CAP_PROP_FRAME_HEIGHT, 540)
+		self.cam2.set(cv2.CAP_PROP_FRAME_WIDTH,800)
+		self.cam2.set(cv2.CAP_PROP_FRAME_HEIGHT, 600)
 		self.cam2.set(cv2.CAP_PROP_FOURCC, int(0x47504A4D))
 
 		rospy.Subscriber('/initiator', Bool, self.initiator)
@@ -68,13 +61,10 @@ class CamPublisher():
 				#		msg=bridge.cv2_to_compressed_imgmsg(image)
 				#		pub.publish(msg)
 			bridge=CvBridge()
-<<<<<<< HEAD
 
 			for i in xrange(4):
 				self.cam.grab()
 				self.cam2.grab()
-=======
->>>>>>> ce6f47b43966ab7e5ad35b1f7c82a5887bdf43f5
 			_, img=self.cam.read()
 			_, img2=self.cam2.read()
 
