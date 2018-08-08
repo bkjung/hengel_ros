ebff2cbac7210b1f2a2712039cf563ac624c325c
bkjung <bkjung94@gmail.com>
2018-08-08 23:08:06 +0900

    ecctransform added

--- hengel_camera/scripts/feature_match.py
+++ hengel_camera/scripts/feature_match.py
@@ -104,6 +104,52 @@ class FeatureMatch():
 
         return M
 
+    def IMAGE_ALIGNMENT_ecc(self, img1, img2):
+        # Find size of image1
+        sz = img1.shape
+        
+        # Define the motion model
+        warp_mode = cv2.MOTION_EUCLIDEAN
+        
+        # Define 2x3 or 3x3 matrices and initialize the matrix to identity
+        if warp_mode == cv2.MOTION_HOMOGRAPHY :
+            warp_matrix = np.eye(3, 3, dtype=np.float32)
+        else :
+            warp_matrix = np.eye(2, 3, dtype=np.float32)
+        
+        # Specify the number of iterations.
+        number_of_iterations = 5000;
+        
+        # Specify the threshold of the increment
+        # in the correlation coefficient between two iterations
+        termination_eps = 1e-10;
+        
+        # Define termination criteria
+        criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, number_of_iterations,  termination_eps)
+        
+        # Run the ECC algorithm. The results are stored in warp_matrix.
+        (cc, warp_matrix) = cv2.findTransformECC (img1,img2,warp_matrix, warp_mode, criteria)
+        
+        if warp_mode == cv2.MOTION_HOMOGRAPHY :
+            # Use warpPerspective for Homography 
+            img2_aligned = cv2.warpPerspective (img2, warp_matrix, (sz[1],sz[0]), flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP)
+        else :
+            # Use warpAffine for Translation, Euclidean and Affine
+            img2_aligned = cv2.warpAffine(img2, warp_matrix, (sz[1],sz[0]), flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP);
+        
+        # Show final results
+        plt.figure(1, figsize=(10,20))
+        plt.subplot(311)
+        plt.imshow(img1, cmap='gray')
+        plt.subplot(312)
+        plt.imshow(img2, cmap='gray')
+        plt.subplot(313)
+        plt.imshow(img2_aligned, cmap='gray')
+        file_time = time.strftime("%y%m%d_%H%M%S")
+        plt.savefig(self.folder_path+"/IMAGE_ALIGN_ecc_"+file_time+".png")
+        plt.close("all")
+        print("IMAGE ALIGN Ecc Saved to "+file_time)
+
 
     def SIFT_FLANN_matching(self, img1, img2):
         plt.figure(1, figsize=(10, 20))
