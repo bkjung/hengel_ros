ca6a8d8a4d85c563e77cb4638526b70417a2cb58
hskim617 <hskim617@gmail.com>
2018-08-29 01:01:01 +0900

    0.295 t0 0.29 & 0.205 to 0.21 by hskim

--- hengel_navigation/scripts/navigation_control.py
+++ hengel_navigation/scripts/navigation_control.py
@@ -1002,8 +1002,8 @@ class NavigationControl():
         arr_error = []
         _a = _delX*sin(_th) - _delY*cos(_th)
         _b = _delX*cos(_th) + _delY*sin(_th)
-        x1 = 0.205
-        x2 = 0.295
+        x1 = 0.21
+        x2 = 0.29
         x3 = 0
         error = 0
         y1 = self.derivative_cost_function(x1, _a, _b, _b-_prev_D, _prev_delOmega1, _prev_delOmega2)
@@ -1018,21 +1018,21 @@ class NavigationControl():
             y1 = self.derivative_cost_function(x1, _a, _b, _b-_prev_D, _prev_delOmega1, _prev_delOmega2)
             y2 = self.derivative_cost_function(x2, _a, _b, _b-_prev_D, _prev_delOmega1, _prev_delOmega2)
         f_x3 = self.cost_function(x3, _a, _b, _b-_prev_D, _prev_delOmega1, _prev_delOmega2)
-        f_low = self.cost_function(0.205, _a, _b, _b-_prev_D, _prev_delOmega1, _prev_delOmega2)
-        f_high = self.cost_function(0.295, _a, _b, _b-_prev_D, _prev_delOmega1, _prev_delOmega2)
+        f_low = self.cost_function(0.21, _a, _b, _b-_prev_D, _prev_delOmega1, _prev_delOmega2)
+        f_high = self.cost_function(0.29, _a, _b, _b-_prev_D, _prev_delOmega1, _prev_delOmega2)
         if f_x3 <= f_low and f_x3 <= f_high:
-            if x3 >= 0.205 and x3 <= 0.295:
+            if x3 >= 0.21 and x3 <= 0.29:
                 return self.bound_function(0.0023, x3, _prev_D)
             else:
                 if f_low <= f_high:
-                    return self.bound_function(0.0023, 0.205, _prev_D)
+                    return self.bound_function(0.0023, 0.21, _prev_D)
                 else:
-                    return self.bound_function(0.0023, 0.295, _prev_D)
+                    return self.bound_function(0.0023, 0.29, _prev_D)
         else:
             if f_low <= f_high:
-                return self.bound_function(0.0023, 0.205, _prev_D)
+                return self.bound_function(0.0023, 0.21, _prev_D)
             else:
-                return self.bound_function(0.0023, 0.295, _prev_D)
+                return self.bound_function(0.0023, 0.29, _prev_D)
 
     #def control_motors(self, _loop_cnt, delta1, delta2):
 
