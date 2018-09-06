ae3601fb059c798ff987e4e23e2f3250d08760db
Mingg8 <mingg9688@gmail.com>
2018-09-07 04:54:10 +0900

    optitrack calibration

--- hengel_navigation/scripts/optitrack_coordinate_calibration.py
+++ hengel_navigation/scripts/optitrack_coordinate_calibration.py
@@ -6,7 +6,6 @@ from std_msgs.msg import Float32, Time, Int32
 from sensor_msgs.msg import Image, CompressedImage
 from nav_msgs.msg import Path
 from visualization_msgs.msg import Marker
-from hengel_optigation.msg import ValveInput, OperationMode
 import tf
 from math import radians, copysign, sqrt, pow, pi, atan2, sin, floor, cos, asin, ceil
 from tf.transformations import euler_from_quaternion
@@ -23,74 +22,56 @@ package_base_path = os.path.abspath(
         os.path.join(os.path.dirname(__file__), "../.."))
 
 class Optitrack():
-    def __init__(self, file_optii, file_opti):
+    def __init__(self, path_navi, path_opti):
         self.R=None
         self.pnt_navi_0=None
         self.pnt_opti_0=None
         self.isInitialized= False
 
-        self.newFile=open(package_base_path+"/hengel_navigation/optitrack_"+time.strftime("%y%m%d_%H%M%S")+".txt", "w"))
+        self.newFile=open(package_base_path+"/hengel_navigation/optitrack_"+time.strftime("%y%m%d_%H%M%S")+".txt", "w")
 
-        with open(file_navi, "r"):
+        with open(path_navi, "r") as file_navi:
             self.arr_navi=[]
             for idx, line in enumerate(file_navi):
-                _str=line.split()
-                self.arr_navi.append([float(_str[0]), float(_str[1]), _str[2]])
-        with open(file_opti):
+                try:
+                    _str=line.split()
+                    self.arr_navi.append([float(_str[0]), float(_str[1]), _str[2]])
+                except Exception as e:
+                    print(e)
+                    print(idx, line)
+        with open(path_opti) as file_opti:
             self.arr_opti=[]
             for idx, line in enumerate(file_opti):
                 _str=line.split()
-                self.arr_opti.append([float(_str[5]), float(_str[7]), _str[0]])
+                # self.arr_opti.append([float(_str[5]), float(_str[7]), _str[0]])
+                self.arr_opti.append([float(_str[0]), float(_str[1]), _str[2]])
+
+        self.run()
 
     # def coordinate_calibration(self, pnt_navi_0, pnt_navi_1, pnt_opti_0, pnt_opti_1):
     def coordinate_calibration(self, index_navi, index_opti):
-        self.pnt_navi_0_x=arr_navi[index_navi][0]
-        self.pnt_navi_0_y=arr_navi[index_navi][1]
+        print("CALIBRATE")
+        self.isInitialized=True
+        self.pnt_navi_0_x=self.arr_navi[index_navi][0]
+        self.pnt_navi_0_y=self.arr_navi[index_navi][1]
 
-        self.pnt_opti_0_x=arr_opti[index_opti][0]
-        self.pnt_opti_0_y=arr_opti[index_opti][1]
+        self.pnt_opti_0_x=self.arr_opti[index_opti][0]
+        self.pnt_opti_0_y=self.arr_opti[index_opti][1]
         
-        theta_navi= atan2(arr_navi[index_navi+1][1]-self.pnt_navi_0_y, arr_navi[index_navi+1][0]-self.pnt_navi_0_x)
-        theta_opti= atan2(arr_opti[index_opti+1][1]-self.pnt_opti_0_y, arr_opti[index_opti+1][0]-self.pnt_opti_0_x)
+        theta_navi= atan2(self.arr_navi[index_navi+1][1]-self.pnt_navi_0_y, self.arr_navi[index_navi+1][0]-self.pnt_navi_0_x)
+        theta_opti= atan2(self.arr_opti[index_opti+1][1]-self.pnt_opti_0_y, self.arr_opti[index_opti+1][0]-self.pnt_opti_0_x)
 
         delta_theta=theta_navi - theta_opti
         
         self.R=[[cos(delta_theta), -sin(delta_theta)],[sin(delta_theta), cos(delta_theta)]]
 
-    def opti_to_navi(self, opti_pnt):
-        x= np.matmul(R, [opti_pnt.x-self.pnt_opti_0.x, opti_pnt.y-self.pnt_opti_0.y])[0]+self.pnt_navi_0.x
-        y= np.matmul(R, [opti_pnt.x-self.pnt_opti_0.x, opti_pnt.y-self.pnt_opti_0.y])[1]+self.pnt_navi_0.y
 
-        return Point(x,y,0)
-
-
-    def run(self):
-        index_navi=0
-        index_opti=0
-
-        while True:
-            while not self.isInitialized:
-                t0=self.arr_opti[index_navi][2]
-                t1=self.arr_navi[index_opti][2]
-
-        self.pnt_navi_0=pnt_navi_0
-        self.pnt_opti_0=pnt_opti_0
-        
-        theta_navi= atan2(pnt_navi_1.y-pnt_navi_1.x)
-        theta_opti= atan2(pnt_opti_1.y-pnt_opti_1.x)
-
-        delta_theat=theta_navi - theta_opti
-        
-        self.R=[[cos(delta_theta), -sin(delta_theta)],[sin(delta_theta), cos(delta_theta)]]
-
-    def opti_to_navi(self, index_navi, index_opti):
+    def opti_to_navi(self, index_opti):
         opti_pnt_x=self.arr_opti[index_opti][0]
         opti_pnt_y=self.arr_opti[index_opti][1]
-        navi_pnt_x=self.arr_navi[index_navi][0]
-        navi_pnt_y=self.arr_navi[index_navi][1]
 
-        x= np.matmul(R, [opti_pnt_x-self.pnt_opti_0.x, opti_pnt_y-self.pnt_opti_0.y])[0]+self.pnt_navi_0.x
-        y= np.matmul(R, [opti_pnt_x-self.pnt_opti_0.x, opti_pnt_y-self.pnt_opti_0.y])[1]+self.pnt_navi_0.y
+        x= np.matmul(self.R, [opti_pnt_x-self.pnt_opti_0_x, opti_pnt_y-self.pnt_opti_0_y])[0]+self.pnt_navi_0_x
+        y= np.matmul(self.R, [opti_pnt_x-self.pnt_opti_0_x, opti_pnt_y-self.pnt_opti_0_y])[1]+self.pnt_navi_0_y
 
         return [x,y]
     
@@ -108,35 +89,67 @@ class Optitrack():
         else:
             return False
 
+    def diffT1T2(self,t1,t2):
+        t1_int= int(t1)
+        t2_int= int(t2)
+        if len(t1)<19:
+            num=19-len(t1)
+            t1_int=t1_int*pow(10,num)
+        if len(t2)<19:
+            num=19-len(t2)
+            t2_int=t2_int*pow(10,num)
+        return t1_int - t2_int
+
     
     def run(self):
         index_navi=0
         index_opti=0
 
-        while True:
+        while index_opti+1<len(self.arr_opti) and index_navi+1<len(self.arr_navi):
+            print("DEBUG0")
             while not self.isInitialized:
-                t_navi_0=self.arr_opti[index_navi][2]
-                t_navi_1=self.arr_opti[index_navi+1][2]
-                t_opti=self.arr_navi[index_opti][0]
-    
-                if isT1Bigger(t_navi_0, t_opti):
+                print("DEBUG1")
+                t_navi_0=self.arr_navi[index_navi][2]
+                t_navi_1=self.arr_navi[index_navi+1][2]
+                t_opti=self.arr_opti[index_opti][2]
+        
+                if self.diffT1T2(t_navi_0, t_opti)>0:
                     index_opti+=1
-                elif isT1Bigger(t1, t_navi_1):
+                elif self.diffT1T2(t_opti, t_navi_1)<0:
                     index_navi+=1
                 else:
                     self.coordinate_calibration(index_navi, index_opti)
-            
-            t_navi_0=self.arr_opti[index_navi][2]
-            t_navi_1=self.arr_opti[index_navi+1][2]
-            t_opti=self.arr_navi[index_opti][0]
+                    index_opti+=1
+                    index_navi+=1
 
-            if isT1Bigger(t_navi_0, t_opti):
+            print("DEBUG2")
+            
+            t_navi_0=self.arr_navi[index_navi][2]
+            t_navi_1=self.arr_navi[index_navi+1][2]
+            t_opti=self.arr_opti[index_opti][2]
+
+            if self.diffT1T2(t_navi_0, t_opti)>0:
                 index_opti+=1
-            elif isT1Bigger(t1, t_navi_1):
+            elif self.diffT1T2(t_opti, t_navi_1)<0:
                 index_navi+=1
             else:
-                x, y = self.opti_to_navi(index_navi, index_opti)
-                self.file.write(str(x)+"\t"+str(y)+"\n")
+                x_opti, y_opti = self.opti_to_navi(index_opti)
+
+                x0=self.arr_navi[index_navi][0]
+                y0=self.arr_navi[index_navi][1]
+
+                x1=self.arr_navi[index_navi+1][0]
+                y1=self.arr_navi[index_navi+1][1]
+
+                t=self.diffT1T2(t_navi_1, t_navi_0)
+                tt=self.diffT1T2(t_opti, t_navi_0)
+
+                x_navi= x0+(x1-x0)/float(t)*tt
+                y_navi= y0+(y1-y0)/float(t)*tt
+
+                self.newFile.write(str(x_navi-x_opti)+"\t"+str(y_navi-y_opti)+"\t"+str(t_opti)+"\n")
+                index_opti+=1
+                index_navi+=1
         
 
 if __name__=="__main__":
