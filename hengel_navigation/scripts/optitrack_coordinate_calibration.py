7be92c487a841e85c723f5c1252c0dc349533747
Mingg8 <mingg9688@gmail.com>
2018-09-09 03:51:56 +0900

    developing

--- hengel_navigation/scripts/optitrack_coordinate_calibration.py
+++ hengel_navigation/scripts/optitrack_coordinate_calibration.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 import rospy
 from geometry_msgs.msg import Twist, Point, Quaternion, PoseStamped
 from std_msgs.msg import Float32, Time, Int32
@@ -47,35 +46,18 @@ class Optitrack():
             for idx, line in enumerate(file_opti):
                 _str=line.split()
                 if len(_str)==9:
-                    self.arr_opti.append([float(_str[5]), float(_str[7]), _str[0]])
+                    self.arr_opti.append([float(_str[5])/10, float(_str[7])/10, _str[0]])
                 # self.arr_opti.append([float(_str[0]), float(_str[1]), _str[2]])
 
         self.run()
-
-    # def coordinate_calibration(self, pnt_navi_0, pnt_navi_1, pnt_opti_0, pnt_opti_1):
-    def coordinate_calibration(self, index_navi, index_opti):
-        print("CALIBRATE")
-        self.isInitialized=True
-        self.pnt_navi_0_x=self.arr_navi[index_navi][0]
-        self.pnt_navi_0_y=self.arr_navi[index_navi][1]
-
-        self.pnt_opti_0_x=self.arr_opti[index_opti][0]
-        self.pnt_opti_0_y=self.arr_opti[index_opti][1]
-
-        theta_navi= atan2(self.arr_navi[index_navi+1][1]-self.pnt_navi_0_y, self.arr_navi[index_navi+1][0]-self.pnt_navi_0_x)
-        theta_opti= atan2(self.arr_opti[index_opti+1][1]-self.pnt_opti_0_y, self.arr_opti[index_opti+1][0]-self.pnt_opti_0_x)
-
-        delta_theta=theta_navi - theta_opti
-
-        self.R=[[cos(delta_theta), -sin(delta_theta)],[sin(delta_theta), cos(delta_theta)]]
 
 
     def opti_to_navi(self, index_opti):
         opti_pnt_x=self.arr_opti[index_opti][0]
         opti_pnt_y=self.arr_opti[index_opti][1]
 
-        x= np.matmul(self.R, [opti_pnt_x-self.pnt_opti_0_x, opti_pnt_y-self.pnt_opti_0_y])[0]+self.pnt_navi_0_x
-        y= np.matmul(self.R, [opti_pnt_x-self.pnt_opti_0_x, opti_pnt_y-self.pnt_opti_0_y])[1]+self.pnt_navi_0_y
+        x= np.matmul(self.R, [opti_pnt_x-self.opti_0_x, opti_pnt_y-self.opti_0_y])[0]+self.navi_0_x
+        y= np.matmul(self.R, [opti_pnt_x-self.opti_0_x, opti_pnt_y-self.opti_0_y])[1]+self.navi_0_y
 
         return [x,y]
 
@@ -94,69 +76,95 @@ class Optitrack():
             return False
 
     def diffT1T2(self,t1,t2):
-        t1_int= int(t1)
-        t2_int= int(t2)
-        if len(t1)<21:
-            num=21-len(t1)
-            t1_int=t1_int*pow(10,num)
-        if len(t2)<21:
-            num=21-len(t2)
-            t2_int=t2_int*pow(10,num)
+        t1_int = int(t1[:10])*pow(10,9)+int(t1[10:])
+        t2_int = int(t1[:10])*pow(10,9)+int(t1[10:])
         return t1_int - t2_int
 
+    # def coordinate_calibration(self, pnt_navi_0, pnt_navi_1, pnt_opti_0, pnt_opti_1):
+    def coordinate_calibration(self, index_navi, index_opti):
+        print("CALIBRATE")
+        self.isInitialized=True
+
+        index_navi, index_opti= self.findTimeStamp(index_navi, index_opti)
+        self.navi_0_x , self.navi_0_y = self.interpolation(index_navi)
+        self.opti_0_x=self.arr_opti[index_opti][0]
+        self.opti_0_y=self.arr_opti[index_opti][1]
+        index_opti +=20
+
+        index_navi, index_opti = self.findTimeStamp(index_navi, index_opti)
+        self.navi_1_x , self.navi_1_y = self.interpolation(index_navi)
+
+
+        theta_navi= atan2(self.navi_1_y-self.navi_0_y, self.navi_1_x-self.navi_0_x)
+        theta_opti= atan2(self.arr_opti[index_opti+1][1]-self.opti_0_y, self.arr_opti[index_opti+1][0]-self.opti_0_x)
+        
+
+        delta_theta=theta_navi - theta_opti
+
+        self.R=[[cos(delta_theta), - sin(delta_theta)],[sin(delta_theta), cos(delta_theta)]]
+        index_opti+=1
+
+        opti_0_x, opti_0_y = self.opti_to_navi(index_opti-21)
+        self.newFile.write(str(self.navi_0_x)+"\t"+str(self.navi_0_y)+"\t"+ str(opti_0_x)+"\t"+str(opti_0_y)+"\n")
+        opti_1_x, opti_1_y=self.opti_to_navi(index_opti-1)
+        self.newFile.write(str(self.navi_1_x)+"\t"+str(self.navi_1_y)+"\t"+ str(opti_1_x)+"\t"+str(opti_1_y)+"\n")
+        
+        return index_navi, index_opti
 
     def run(self):
         index_navi=0
         index_opti=0
 
         while index_opti+1<len(self.arr_opti) and index_navi+1<len(self.arr_navi):
-            while not self.isInitialized and index_opti+1<len(self.arr_opti) and index_navi+1<len(self.arr_navi):
-                t_navi_0=self.arr_navi[index_navi][2]
-                t_navi_1=self.arr_navi[index_navi+1][2]
-                t_opti=self.arr_opti[index_opti][2]
+            if not self.isInitialized:
+                index_navi, index_opti = self.coordinate_calibration(index_navi, index_opti)
 
-                if self.diffT1T2(t_navi_0, t_opti)>0:
-                    print(self.diffT1T2(t_navi_0, t_opti))
-                    index_opti+=1
-                elif self.diffT1T2(t_opti, t_navi_1)>0:
-                    index_navi+=1
-                else:
-                    self.coordinate_calibration(index_navi, index_opti)
-                    index_opti+=1
-                    index_navi+=1
+            self.findTimeStamp(index_navi, index_opti)
+            # x_opti, y_opti = self.opti_to_navi(index_opti)
+            x_navi, y_navi = self.interpolation(index_navi)
 
 
-            t_navi_0=self.arr_navi[index_navi][2]
-            t_navi_1=self.arr_navi[index_navi+1][2]
-            t_opti=self.arr_opti[index_opti][2]
 
-            if self.diffT1T2(t_navi_0, t_opti)>0:
-                index_opti+=1
-            elif self.diffT1T2(t_opti, t_navi_1)>0:
-                index_navi+=1
-            else:
-                x_opti, y_opti = self.opti_to_navi(index_opti)
+            #######33 DEBUG ############
+            x_opti=self.arr_opti[index_opti][0]
+            y_opti=self.arr_opti[index_opti][1]
 
-                x0=self.arr_navi[index_navi][0]
-                y0=self.arr_navi[index_navi][1]
 
-                x1=self.arr_navi[index_navi+1][0]
-                y1=self.arr_navi[index_navi+1][1]
-
-                t=self.diffT1T2(t_navi_1, t_navi_0)
-                tt=self.diffT1T2(t_opti, t_navi_0)
-
-                x_navi= x0+(x1-x0)/float(t)*tt
-                y_navi= y0+(y1-y0)/float(t)*tt
-
-                self.newFile.write(str(x_navi-x_opti)+"\t"+str(y_navi-y_opti)+"\t"+str(x_opti)+"\t"+str(y_opti)+"\t"+str(t_opti)+"\t"+str(t_navi_0)+"\t"+str(t_navi_1)+"\n")
-                index_opti+=1
-                index_navi+=1
+            # self.newFile.write(str(x_navi-x_opti)+"\t"+str(y_navi-y_opti)+"\t"+str(x_opti)+"\t"+str(y_opti)+"\t"+str(self.t_opti)+"\t"+str(self.t_navi_0)+"\t"+str(self.t_navi_1)+"\n")
+            self.newFile.write(str(x_navi)+"\t"+str(y_navi)+"\t"+ str(x_opti)+"\t"+str(y_opti)+"\t"+str(self.t_opti)+"\t"+str(self.t_navi_0)+"\t"+str(self.t_navi_1)+"\n")
+            index_opti+=1
 
             #x,y=self.opti_to_navi(index_opti)
             #index_opti+=1
             #index_navi+=1
             #self.newFile.write(str(x)+"\t"+str(y)+"\n")
+    
+    def findTimeStamp(self, index_navi, index_opti):
+        while index_opti+1<len(self.arr_opti) and index_navi +1 <len(self.arr_navi):
+            self.t_navi_0=self.arr_navi[index_navi][2]
+            self.t_navi_1=self.arr_navi[index_navi+1][2]
+            self.t_opti=self.arr_opti[index_opti][2]
+
+            if self.diffT1T2(self.t_navi_0, self.t_opti)>0:
+                index_opti+=1
+            elif self.diffT1T2(self.t_opti, self.t_navi_1)>0:
+                index_navi+=1
+            else:
+                return index_navi, index_opti
+    
+    def interpolation(self, index_navi):
+        x0=self.arr_navi[index_navi][0]
+        y0=self.arr_navi[index_navi][1]
+
+        x1=self.arr_navi[index_navi+1][0]
+        y1=self.arr_navi[index_navi+1][1]
+
+        t=self.diffT1T2(self.t_navi_1, self.t_navi_0)
+        tt=self.diffT1T2(self.t_opti, self.t_navi_0)
+
+        x_navi= x0+(x1-x0)/float(t)*tt
+        y_navi= y0+(y1-y0)/float(t)*tt
+        return x_navi, y_navi
 
 
 if __name__=="__main__":
